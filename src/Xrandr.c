84264cf7a9ce88850f3bc18428e2c046ff0251ed
Markus Kuhn <Markus.Kuhn@cl.cam.ac.uk>
2004-12-04 00:42:48 +0000

    Encoding of numerous files changed to UTF-8

--- src/Xrandr.c
+++ src/Xrandr.c
@@ -1,8 +1,8 @@
 /*
  * $XFree86: xc/lib/Xrandr/Xrandr.c,v 1.13tsi Exp $
  *
- * Copyright © 2000 Compaq Computer Corporation, Inc.
- * Copyright © 2002 Hewlett Packard Company, Inc.
+ * Copyright Â© 2000 Compaq Computer Corporation, Inc.
+ * Copyright Â© 2002 Hewlett Packard Company, Inc.
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
